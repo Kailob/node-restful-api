1dd372a5eb68ada8c7b07ebfb0c9d154b700bd67
Miguel Tavares <mgl.tvrs@gmail.com>
2023-03-18 21:35:59 +0000

    Automatic reload, default logging & error handling

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
 const productsRoutes = require('./routes/products');
 const ordersRoutes = require('./routes/orders');
 
 // Parses incoming requests with JSON payloads and is based on body-parser.
 app.use(express.json())
 
-// Product Routes
+// Logger middleware, for development.
+app.use(morgan('dev'));
+
+// App Routes
 app.use('/products', productsRoutes);
 app.use('/orders', ordersRoutes);
 
+// Request didn't match previous routes
+app.use((req, res, next) => {
+    const error = new Error('Route Not Found');
+    error.status = 404;
+    next(error);
+});
+
+// Captures any error thrown by the app
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message || 'Generic Error'
+        }
+    });
+});
 
 module.exports = app;
