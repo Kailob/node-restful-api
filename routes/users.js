777ade7004b56646bd816055080f513375f385e7
Miguel Tavares <mgl.tvrs@gmail.com>
2023-03-22 19:17:35 +0000

    User Sign-up improvements

--- routes/users.js
+++ routes/users.js
@@ -8,44 +8,51 @@ const SALT = bcrypt.genSaltSync(process.env.AUTH_SALT_ROUNDS || 10);
 router.post('/sign-up', (req, res, next) => {
     const { email, password } = req.body;
 
-    const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        email,
-        password: bcrypt.hashSync(
-            password,
-            SALT,
-            (err, hash) => {
-                if (err) {
-                    return res.status(500).json({
-                        error: err
-                    });
-                }
-            }
-        )
-    });
-
-    user
-        .save()
+    User
+        .find({ email })
+        .exec()
         .then(doc => {
-            console.error(doc);
-            res.status(201).json({
-                newUser: {
-                    _id: doc._id,
-                    email: doc.email,
-                }
-            });
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).json({
-                error: err
-            });
+            if (doc.length >= 1) {
+                res.status(409).json({ message: "Email exists" });
+            } else {
+                bcrypt.hash(
+                    password,
+                    SALT,
+                    (err, hash) => {
+                        if (err) {
+                            return res.status(500).json({
+                                error: err
+                            });
+                        } else {
+                            const user = new User({
+                                _id: new mongoose.Types.ObjectId(),
+                                email,
+                                password: hash
+                            });
+
+                            user
+                                .save()
+                                .then(doc => {
+                                    console.error(doc);
+                                    res.status(201).json({
+                                        newUser: {
+                                            _id: doc._id,
+                                            email: doc.email,
+                                        }
+                                    });
+                                })
+                                .catch(err => {
+                                    console.error(err);
+                                    res.status(500).json({
+                                        error: err
+                                    });
+                                });
+                        }
+                    }
+                )
+            }
         });
-
-    // Another way to go around it is to use bcrypt.hash and only save the user if there's no error
 });
-
-
 
 // GET all users. Just for testing purposes
 router.get('/', (req, res, next) => {
